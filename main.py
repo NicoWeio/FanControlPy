2ac6141156953fc7596501b12cbce70773ed8ab9
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-04-07 18:35:00 +0200

    moving average for temperature

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ from fancontrolpy.fan import Fan
 from fancontrolpy.fan_curve import FanCurve
 from fancontrolpy.temp_sensor import TempSensor
 
+# how often to update the fan speed
+INTERVAL_SECONDS = 5
+# how many seconds to average over
+AVG_SECONDS = 30
+# calculate how many samples to average over
+AVG_SAMPLES = AVG_SECONDS // INTERVAL_SECONDS
+assert AVG_SAMPLES >= 2  # needed for correct array indexing
+
+
 fans = [fan for fan in MY_FANS if fan.fan_curve]
 tempSensor = TempSensor(1)
 
@@ -17,25 +26,30 @@ for fan in fans:
     if not fan.enable:
         print(f"Enabling {fan}")
         try:
-        fan.enable = True
+            fan.enable = True
         except AssertionError:
             # Setting a speed might enable it, though, so don't fail here.
             print(f"Failed to enable {fan}")
 
+last_temps: list[float] = []
+
 try:
-    last_handled_temp = 0  # reasonable default
+    last_handled_temp = 0.0  # reasonable default
     while True:
         temp = tempSensor.temp
-        print(f'Current temperature: {temp:.1f} °C')
-        if abs(temp - last_handled_temp) < 1:
-            print('No significant change in temperature, skipping…')
+        last_temps = last_temps[-(AVG_SAMPLES-1):] + [temp]
+        smooth_temp = sum(last_temps) / len(last_temps)
+        print(f'Current temperature: {temp:.1f} °C, average: {smooth_temp:.1f} °C')
+        if abs(smooth_temp - last_handled_temp) < 1:
+            # print('No significant change in temperature, skipping…')
+            pass
         else:
-            last_handled_temp = temp
+            last_handled_temp = smooth_temp
             for fan in fans:
                 calculatedSpeed = fan.fan_curve.get_power(temp)
                 print(f"{fan}: {calculatedSpeed:.0%}, {fan.rpm} RPM")
                 fan.fan_speed = calculatedSpeed
-        time.sleep(10)
+        time.sleep(INTERVAL_SECONDS)
 
 except KeyboardInterrupt:
     print('\nExiting; resetting fans...')
